d56f1e3b2df81c8071226e2ecab6a732a90361c8
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2023-05-29 22:05:44 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://lktojbpqcnevpvxbgvdg.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrdG9qYnBxY25ldnB2eGJndmRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0MDUxNzIsImV4cCI6MjAwMDk4MTE3Mn0.MDhuZUQ7VkX0g4X0K9cyS5x0P7zcclqrbaghPBrSnD4',
+}
